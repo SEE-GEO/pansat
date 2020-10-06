beadbde10b848d979d76e06a2bbf7bd39565d906
Hannah Imhof <hannah.imhof@chalmers.se>
2020-10-06 14:24:42 +0200

    Fixing lost changes: Readme with badge for install and test workflow and necessary package for test

--- setup.py
+++ setup.py
@@ -14,6 +14,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/SEE-MOF/pansat",
     packages=setuptools.find_packages(),
+    install_requires=[
+        "keyring"
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: GNU Affero",
