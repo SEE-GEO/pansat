33dbfe001d6f9e5ff2da0752451b8cce984f9985
Hannah Imhof <hannah.imhof@chalmers.se>
2020-10-09 17:48:01 +0200

    Removing fake e-mail from setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ setuptools.setup(
     name="pansat",
     version="0.0.1",
     author="Julia Kukulies, Simon Pfreundschuh, Franz Kanngießer, Hannah Imhof",
-    author_email="contact@pansat.com",
     description="Download, extraction, remapping and analysis of satellite and climate data",
     long_description=long_description,
     long_description_content_type="text/markdown",
