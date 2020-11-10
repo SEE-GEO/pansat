4bc091b1257a30b07a7df2d0806e0d7b70badf2e
Juli <julia.kukulies@gu.se>
2020-11-10 08:18:54 +0100

    added NOAA provider

--- pansat/download/providers/noaa.py
+++ pansat/download/providers/noaa.py
@@ -20,25 +20,26 @@ from datetime import datetime, timedelta
 import ftplib
 
 
-
 NOAA_PRODUCTS = ['ncep-reanalysis-surface, ncep-reanalysis-pressure']
-
 
 
 class NOAAProvider(DataProvider):
     """
     Abstract base class for gridded products available from NOAA Physical Science Laboratory.
     """
+    base_url = "ftp://ftp2.psl.noaa.gov/Datasets"
+
     def __init__(self, product):
         """
         Create a new product instance.
 
         Args:
 
-        product(str): product name
+        product_path(str): The path of the product. This should point to
+        the folder that holds yearly .nc files for different variables. 
         """
         super().__init__()
-        self.product = product
+        self.product_path = str(product_path)
 
         if not product.name in NOAA_PRODUCTS:
             available_products = NOAA_PRODUCTS
@@ -59,29 +60,44 @@ class NOAAProvider(DataProvider):
             be downloaded from this data provider.
         """
 
+
+    def get_files_by_year(self, year, day):
+        """
+        Return all files from given year and julian day.
+
+        Args:
+            year(``int``): The year from which to retrieve the filenames.
+   
+        Return:
+            List of the filenames of this product on the given day. """
+
+
+        return files
+
+
+
     @abstractmethod
-    def download(self, start, end, destination=None):
+    def download(self, filename, destination=None):
         """
         This method downloads data for a given time range from respective the
         data provider.
 
         Args:
-            start(``datetime.datetime``): date and time for start
-            end(``datetime.datetime``): date and time for end
+            filename(``str``): The name of the file to download.
             destination(``str`` or ``pathlib.Path``): path to directory where
                 the downloaded files should be stored.
         """
 
+        # target directory
+        path = "/".join([self.base_url, self.product_path])
 
-        path  = Path('ftp://ftp2.psl.noaa.gov/Datasets/ncep.reanalysis/surface/') 
         ftp = ftplib.FTP(path)
-        ftp.login("anonymous", mailpw)
-        ftp.cwd(targetdir)
+        ftp.login(user = "anonymous", passwd= mailpw)
+        ftp.cwd(path)
 
-        with open(filename, 'wb') as fp:
+        with open(destination, 'wb') as fp:
             ftp.retrbinary('RETR ' + filename, fp.write)
             ft.quit()
-
 
 
 
