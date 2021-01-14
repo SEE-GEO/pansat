b2860f6bdfa554c25dc705a4961fe3ae70e05077
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2021-01-14 18:36:26 +0100

    Remote .pytest cache after running tests.

--- .github/actions/weekly_build/entrypoint.sh
+++ .github/actions/weekly_build/entrypoint.sh
@@ -3,6 +3,11 @@ cd /github/workspace/
 ls
 pip install .
 pytest test/unit_tests
+rm -rf .pytest_cache
 
 cd notebooks
 jupyter nbconvert --to notebook *.ipynb
+cd products
+jupyter nbconvert --to notebook *.ipynb
+
+
