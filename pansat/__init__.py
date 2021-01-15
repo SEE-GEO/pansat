c6b99d463fa61f5d4b721972cc71330b2fbce680
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2021-01-15 10:37:30 +0100

    formatting.

--- pansat/__init__.py
+++ pansat/__init__.py
@@ -11,6 +11,4 @@ import logging
 # Set logging level.
 _LOGGING_LEVEL = os.environ.get("PANSAT_LOG_LEVEL", "WARNING")
 _LOG_FORMAT = "{name} ({levelname:10}) :: {message}"
-logging.basicConfig(level=_LOGGING_LEVEL,
-                    format=_LOG_FORMAT,
-                    style="{")
+logging.basicConfig(level=_LOGGING_LEVEL, format=_LOG_FORMAT, style="{")
