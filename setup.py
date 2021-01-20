79982ed4d501c86fcf2757095c9ea93200874865
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2021-01-20 08:40:55 +0100

    Changed version to development and use real pypi as upload destination.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pansat",
-    version="0.0.1",
+    version="0.0.dev1",
     author="Julia Kukulies, Simon Pfreundschuh, Franz Kanngießer, Hannah Imhof",
     description="Download, extraction, remapping and analysis of satellite and climate data",
     long_description=long_description,
