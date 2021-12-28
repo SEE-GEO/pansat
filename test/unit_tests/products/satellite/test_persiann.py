efe6e07074628e19c1fa456168ab9ff05cd5b1bc
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2021-12-29 00:09:04 +0100

    Added provider and products to download PERSIANN precip data.

---
+++ test/unit_tests/products/satellite/test_persiann.py
@@ -0,0 +1,39 @@
+"""
+Tests for the available PERSIANN products.
+"""
+from datetime import datetime, timedelta
+from pathlib import Path
+
+import numpy as np
+
+from pansat.products.satellite.persiann import (CCS, PDIRNow)
+
+def test_ccs(tmp_path):
+    """
+    Test downloading and opening of PERSIANN CCS files.
+    """
+    day = np.random.randint(1, 31)
+    start = datetime(2020, 12, day)
+    product = CCS(1)
+    files = product.download(start, start, tmp_path / "CCS")
+
+    assert Path(files[0]).exists()
+
+    data = product.open(files[0])
+    assert "latitude" in data.dims
+    assert "longitude" in data.dims
+
+def test_pdirnow(tmp_path):
+    """
+    Test downloading and opening of PERSIANN CCS files.
+    """
+    day = np.random.randint(1, 31)
+    start = datetime(2020, 12, day)
+    product = PDIRNow(1)
+    files = product.download(start, start, tmp_path / "PDIRNow")
+
+    assert Path(files[0]).exists()
+
+    data = product.open(files[0])
+    assert "latitude" in data.dims
+    assert "longitude" in data.dims
