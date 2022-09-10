a7972b8c7acb4e8cd7d2c4eed9989db44a9a33ac
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2022-09-10 14:02:42 +0200

    Added time module.

---
+++ pansat/time.py
@@ -0,0 +1,37 @@
+"""
+pansat.time
+===========
+
+This module provides functionality for dealing with times.
+"""
+from datetime import datetime
+from dataclasses import dataclass
+import pandas as pd
+import numpy as np
+
+def to_datetime(time):
+    """
+    Try to convert a given time to a datetime object.
+    """
+    if isinstance(time, datetime):
+        return time
+    try:
+        return pd.Timestamp(time).to_pydatetime()
+    except ValueError:
+        raise ValueError(
+            "Could not convert '%s' to datetime object."
+        )
+
+
+def to_datetime64(time):
+    """
+    Try to convert a given time to a numpy datetime64 object.
+    """
+    if isinstance(time, np.ndarray) and time.dtype == np.datetime64:
+        return time
+    try:
+        return pd.Timestamp(time).to_datetime64()
+    except ValueError:
+        raise ValueError(
+            "Could not convert '%s' to datetime object."
+        )
