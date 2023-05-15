ae84c246f88291b81130d5dcaf46a5568c5f5940
Simon Pfreundschuh <s.pfreundschuh@gmail.com>
2023-05-15 07:53:19 -0600

    Formatted EUMETSAT provider module and added tests.

--- pansat/download/providers/eumetsat.py
+++ pansat/download/providers/eumetsat.py
@@ -139,6 +139,14 @@ class EUMETSATProvider(DataProvider):
 
     @staticmethod
     def get_collections():
+        """
+        Return collections available from the Eumetsat data
+        store.
+
+        Return:
+            A dict mapping names of the available collections to the
+            URLs at which they are available.
+        """
         # b64 = base64.b64encode((key + ":" + secret).encode())
         url = "https://api.eumetsat.int/data/browse/collections"
         req = requests.get(
@@ -166,7 +174,6 @@ class EUMETSATProvider(DataProvider):
         Args:
 
             product(``Product``): Product class object with specific product for ICARE
-
         """
         if str(product) not in _PRODUCTS:
             available_products = list(_PRODUCTS.keys())
@@ -182,6 +189,7 @@ class EUMETSATProvider(DataProvider):
 
     @property
     def token(self):
+        """The identification token for the API."""
         if self._token is None:
             user, password = get_identity("EUMETSAT")
             self._token = AccessToken(user, password)
@@ -191,9 +199,23 @@ class EUMETSATProvider(DataProvider):
         return self._token
 
     def get_available_products():
+        """List of the products available from this provider."""
         return _PRODUCTS.keys()
 
     def get_files_in_range(self, start, end, bounding_box=None):
+        """
+        Return files in available in range.
+
+        Args:
+            start: Start of the time range.
+            end: End of the time range.
+            bounding_box: An optional bounding box specifying a region
+                of interest (ROI). If given, only files containing
+                observations over this regions will be returned.
+
+        Return:
+             A list of URLs of available files.
+        """
         product_id = _PRODUCTS[str(self.product)]
 
         parameters = {
@@ -220,7 +242,6 @@ class EUMETSATProvider(DataProvider):
         while start_index < total_results:
             parameters["si"] = start_index
             with requests.get(url, params=parameters) as r:
-
                 r.raise_for_status()
 
                 datasets = r.json()
@@ -247,7 +268,17 @@ class EUMETSATProvider(DataProvider):
         return links
 
     def download_file(self, link, destination):
+        """
+        Download a specific file.
 
+        Args:
+            link: Link pointing towards the file to download.:
+            destination: Where to store the file.
+
+        Return:
+            The path of the local file to which the file content
+            was written.
+        """
         destination = Path(destination)
         if not destination.exists():
             destination.mkdir(exist_ok=True, parents=True)
@@ -269,6 +300,17 @@ class EUMETSATProvider(DataProvider):
         return dest
 
     def download(self, start, end, destination):
+        """
+        Download all products within a given time range.
+
+        Args:
+            start: Start time
+            end: End time.
+            destination: Path to where to store the downloaded files.
+
+        Return:
+            List of the local paths of all downloaded files.
+        """
         destination = Path(destination)
 
         links = self.get_files_in_range(start, end)
