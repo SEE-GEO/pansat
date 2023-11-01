ce64ce351c148dcc162b6cca3cb8211019cd9dd5
Simon Pfreundschuh <simon.pfreundschuh@colostate.edu>
2023-11-01 17:08:36 -0600

    Add Granule to top-level module.

--- pansat/__init__.py
+++ pansat/__init__.py
@@ -12,6 +12,7 @@ from pansat.download.providers.data_provider import DataProvider
 from pansat.products.product import Product
 from pansat.time import TimeRange
 from pansat.file_record import FileRecord
+from pansat.granule import Granule
 
 # Set logging level.
 _LOGGING_LEVEL = os.environ.get("PANSAT_LOG_LEVEL", "WARNING")
