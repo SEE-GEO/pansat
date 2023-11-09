10fa524f85d2fd5e7254c8f9bd56bd7f19b64ec0
kukulies <kukulies@mmm-aurora.mmm.ucar.edu>
2023-11-08 20:03:15 -0700

    remove providers from init file here, all the action happens in data_provider.py

--- pansat/download/providers/__init__.py
+++ pansat/download/providers/__init__.py
@@ -9,54 +9,5 @@ must be instantiated with a given product object. If the product is available
 from the data provider class, the resulting data provider instance can be used
 to download files of the data product.
 
-Example
--------
-
-.. code-block::
-
-    from datetime import datetime
-    from pansat.download.provider.icare import IcareProvider
-    from pansat.products.satellite.cloud_sat import l1b_cpr
-
-    provider = IcareProvider(l1b_cpr)
-    t_0 = datetime(2016, 11, 21, 10)
-    t_1 = datetime(2016, 11, 21, 12)
-    files = provider.download(t_0, t_1)
-
-Attributes:
-    ALL_PROVIDERS: List containing all available data provider classes.
-        This list should be used by products to determine a data provider to
-        use to download files.
 """
-from pansat.download.providers.data_provider import DataProvider
-from pansat.download.providers.copernicus import CopernicusProvider
-from pansat.download.providers.icare import IcareProvider
-from pansat.download.providers.noaa import NOAAProvider
-from pansat.download.providers.meteo_france import GeoservicesProvider
-from pansat.download.providers.laads_daac import LAADSDAACProvider
-from pansat.download.providers.goes_aws import GOESAWSProvider
-from pansat.download.providers.himawari_aws import HimawariAWSProvider
-from pansat.download.providers.iowa_state import IowaStateProvider
-from pansat.download.providers.eumetsat import EUMETSATProvider
-from pansat.download.providers.uci import UciProvider
-from pansat.download.providers.noaa_ncei import NOAANCEIProvider
-from pansat.download.providers.cloudnet import CloudnetProvider
-from pansat.download.providers.cloudsat_dpc import CloudSatDPCProvider
-import pansat.download.providers.iowa_state
 
-
-ALL_PROVIDERS = [
-    CopernicusProvider,
-    IcareProvider,
-    NOAAProvider,
-    GeoservicesProvider,
-    LAADSDAACProvider,
-    GOESAWSProvider,
-    HimawariAWSProvider,
-    IowaStateProvider,
-    EUMETSATProvider,
-    UciProvider,
-    NOAANCEIProvider,
-    CloudnetProvider,
-    CloudSatDPCProvider,
-]
