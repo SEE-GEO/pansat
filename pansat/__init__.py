f53727c39167fbdec502f0ded01e33641e67cacb
Simon Pfreundschuh <s.pfreundschuh@gmail.com>
2024-01-18 08:16:43 -0700

    Import Product in top-level module.

--- pansat/__init__.py
+++ pansat/__init__.py
@@ -12,6 +12,7 @@ from pansat.download.providers.data_provider import DataProvider
 from pansat.time import TimeRange
 from pansat.file_record import FileRecord
 from pansat.granule import Granule
+from pansat.products import Product
 from pansat.geometry import Geometry
 
 # Set logging level.
