Fix scaling of NOAA GAASP L1B files.

pansat/products/satellite/noaa/gaasp.py
@@ -201,7 +201,9 @@ def load_channels(
     for chan in channels:
         if slices is None:
             slices = slice(0, None)
-        tbs.append(file_handle[chan].__getitem__(slices))
+        scaling = file_handle[chan].attrs["SCALE FACTOR"][0]
+        tbs.append(scaling * file_handle[chan].__getitem__(slices))
+
     tbs = np.stack(tbs, -1)
     return tbs
 

test/unit_tests/products/satellite/noaa/test_gaasp.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import os
 from pathlib import Path
 
+import numpy as np
 import pytest
 
 from pansat.catalog import Index
@@ -39,13 +40,21 @@ def test_get_temporal_coverage():
 @NEEDS_GAASP_DATA
 def test_open():
     """
-    Test opening of AMSR2 files.
+    Test opening of AMSR2 files and ensure that all loaded brightness temperatures
+    are physical.
     """
     files = sorted(list(Path(NOAA_GAASP_DATA).glob("*.h5")))
     data = l1b_gcomw1_amsr2.open(files[0])
     assert "tbs_s1" in data
     assert "tbs_s2" in data
     assert "tbs_s3" in data
+
+    assert np.nanmax(data["tbs_s1"].data) < 400
+    assert np.nanmin(data["tbs_s1"].data) > 0
+    assert np.nanmax(data["tbs_s2"].data) < 400
+    assert np.nanmin(data["tbs_s2"].data) > 0
+    assert np.nanmax(data["tbs_s3"].data) < 400
+    assert np.nanmin(data["tbs_s3"].data) > 0
 
 
 @NEEDS_GAASP_DATA