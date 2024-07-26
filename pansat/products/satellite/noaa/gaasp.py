62ae07673cd1e2c3a78db941afbaa644257237e1
Simon Pfreundschuh <s.pfreundschuh@gmail.com>
2024-07-26 03:14:44 -0600

    Added NOAA GAASP product class for handling AMSR2 L1B files.

---
+++ pansat/products/satellite/noaa/gaasp.py
@@ -0,0 +1,304 @@
+"""
+pansat.products.satellite.noaa.gaasp
+====================================
+
+Provides a pansat product for the AMSR2 L1B files produced by
+the GCOM-W1 AMSR2 Algorithm Software Package.
+"""
+from datetime import datetime
+from pathlib import Path
+import re
+from typing import List, Optional, Tuple
+
+import h5py
+import numpy as np
+import xarray as xr
+
+import pansat
+from pansat.file_record import FileRecord
+from pansat.time import TimeRange
+from pansat.granule import Granule
+from pansat.products import GranuleProduct, FilenameRegexpMixin
+from pansat.products.product_description import ProductDescription
+from pansat import geometry
+
+
+class GAASPProduct(FilenameRegexpMixin, GranuleProduct):
+    """
+    Class representing GAASP products.
+    """
+
+    def __init__(self):
+        self._name = "l1b_gw1_amsr2"
+        module_path = Path(__file__).parent
+        self.product_description = ProductDescription(
+            module_path / "l1b_gaasp_gw1_amsr2.ini"
+        )
+        self.filename_regexp = re.compile(
+            rf"GAASP-L1B_v\dr\d_GW1_s(\d*)_e(\d*)_c(\d*).h5"
+        )
+        GranuleProduct.__init__(self)
+
+    @property
+    def name(self) -> str:
+        """
+        The product name that uniquely identifies the product within pansat.
+        """
+        module = Path(__file__).parent
+        root = Path(pansat.products.__file__).parent
+        prefix = str(module.relative_to(root)).replace("/", ".")
+        return ".".join([prefix, self._name])
+
+    def filename_to_date(self, filename):
+        """
+        Extract timestamp from filename.
+
+        Args:
+            filename(``str``): Filename of a GPM product.
+
+        Returns:
+            ``datetime`` object representing the timestamp of the
+            filename.
+        """
+        path = Path(filename)
+        match = self.filename_regexp.match(path.name)
+
+        # Some files of course have to follow a different convention.
+        if match is None:
+            date_string = "20" + path.name.split("_")[2]
+        else:
+            date_string = match.group(2) + match.group(3)
+        date = datetime.strptime(date_string, "%Y%m%d%H%M%S")
+
+        return date
+
+    def get_temporal_coverage(self, rec: FileRecord) -> TimeRange:
+        """
+        Implements interface to extract temporal coverage of file.
+        """
+        if isinstance(rec, (str, Path)):
+            rec = FileRecord(rec)
+        match = self.filename_regexp.match(rec.filename)
+        if match is None:
+            raise RuntimeError(
+                f"Provided file record with filename {rec.filename} doest not "
+                " match the products filename regexp "
+                f"{self.filename_regexp.pattern}. "
+            )
+
+        start_date = match.group(1)
+        start_date = datetime.strptime(start_date, "%Y%m%d%H%M%S0")
+        end_date = match.group(2)
+        end_date = datetime.strptime(end_date, "%Y%m%d%H%M%S0")
+        return TimeRange(start_date, end_date)
+
+    def get_spatial_coverage(self, rec: FileRecord) -> geometry.Geometry:
+        """
+        Implements interface to extract spatial coverage of file.
+        """
+        if rec.local_path is None:
+            raise ValueError(
+                "This products reuqires a local file is to determine "
+                " the spatial coverage."
+            )
+
+        with HDF5File(rec.local_path, "r") as file_handle:
+            lons, lats = self.product_description.load_lonlats(file_handle, slice(0, None, 1))
+        poly = geometry.parse_swath(lons, lats, m=10, n=1)
+        return geometry.ShapelyGeometry(poly)
+
+    @property
+    def default_destination(self):
+        """
+        The default destination for GPM product is
+        ``GPM/<product_name>``>
+        """
+        return Path("gpm") / self.level / self.platform / self.sensor
+
+    def __str__(self):
+        return self.name
+
+    def open(self, rec: FileRecord, slcs: Optional[dict[str, slice]] = None):
+        """
+        Open file as xarray dataset.
+
+        Args:
+            rec: A FileRecord whose local_path attribute points to a local
+                GPM file to open.
+            slcs: An optional dictionary of slices to use to subset the
+                data to load.
+
+        Return:
+            An xarray.Dataset containing the loaded data.
+        """
+        from pansat.formats.hdf5 import HDF5File
+
+        if isinstance(rec, (str, Path)):
+            rec = FileRecord(rec)
+
+        with HDF5File(rec.local_path, "r") as file_handle:
+            return self.product_description.to_xarray_dataset(
+                file_handle, context=globals(), slcs=slcs
+            )
+
+    def get_granules(self, rec: FileRecord) -> List[Granule]:
+        """
+        Get granules describing spatiotemporal coverage of file.
+
+        Args:
+            rec: A file record pointing to a file from which to extract granules.
+        """
+        from pansat.formats.hdf5 import HDF5File
+
+        if not isinstance(rec, FileRecord):
+            rec = FileRecord(rec)
+
+        granules = []
+        with HDF5File(rec.local_path, "r") as file_handle:
+            for granule_data in self.product_description.get_granule_data(
+                file_handle, globals()
+            ):
+                granules.append(Granule(rec, *granule_data))
+        return granules
+
+    def open_granule(self, granule: Granule) -> xr.Dataset:
+        """
+        Load data from granule.
+
+        Args:
+            granule: The granule for which to load the data
+
+        Return:
+            An xarray.Dataset containing the loaded data.
+        """
+        from pansat.formats.hdf5 import HDF5File
+
+        filename = granule.file_record.local_path
+        with HDF5File(filename, "r") as file_handle:
+            return self.product_description.to_xarray_dataset(
+                file_handle, context=globals(), slcs=granule.get_slices()
+            )
+
+
+def load_channels(
+        file_handle: h5py.File,
+        channels: List[str],
+        slices: Optional[Tuple[slice]] = None
+) -> np.ndarray:
+    """
+    Load variables from HDF5 file handle and stack along last dimension.
+
+    Args:
+        file_handle: The file handle to the opened HDF5 file containing the L1B data.
+        channels: List of the channels names to load.
+        slices: An iptional tuple containing slices to subset the data to load.
+
+    Return:
+        The loaded data as an np.ndarray.
+    """
+
+    tbs = []
+    for chan in channels:
+        if slices is None:
+            slices = slice(0, None)
+        tbs.append(file_handle[chan].__getitem__(slices))
+    tbs = np.stack(tbs, -1)
+    return tbs
+
+
+def load_tbs_low_res_amsr2(
+        file_handle: h5py.File,
+        slices: Optional[Tuple[slice]] = None
+):
+    """
+    Load Tbs from AMSR2 low-resolution channels.
+
+    Args:
+        file_handle: File handle pointing to the opened HDF5 file from which
+            to load the data.
+        slices: Optional tuples of slices to use to subset the data to load.
+
+    Return:
+        A numpy.ndarray containing the loaded data.
+    """
+    low_res_chans = [
+        "6.9GHz,H", "6.9GHz,V",
+        "10.7GHz,H", "10.7GHz,V",
+        "18.7GHz,H", "18.7GHz,V",
+        "23.8GHz,H", "23.8GHz,V",
+        "36.5GHz,H", "36.5GHz,V",
+        "7.3GHz,H", "7.3GHz,V",
+    ]
+    low_res_chans = [
+        f"Brightness Temperature ({freq})" for freq in low_res_chans
+    ]
+    return load_channels(file_handle, low_res_chans, slices=slices)
+
+
+def load_tbs_89a_amsr2(
+        file_handle: h5py.File,
+        slices: Optional[Tuple[slice]] = None
+):
+    """
+    Load Tbs from AMSR2 89GHz-A channels.
+
+    Args:
+        file_handle: File handle pointing to the opened HDF5 file from which
+            to load the data.
+        slices: Optional tuples of slices to use to subset the data to load.
+
+    Return:
+        A numpy.ndarray containing the loaded data.
+    """
+    chans = [
+        "89.0GHz-A,H", "89.0GHz-A,V",
+    ]
+    chans = [
+        f"Brightness Temperature ({freq})" for freq in chans
+    ]
+    return load_channels(file_handle, chans, slices=slices)
+
+
+def load_tbs_89b_amsr2(
+        file_handle: h5py.File,
+        slices: Optional[Tuple[slice]] = None
+):
+    """
+    Load Tbs from AMSR2 89GHz-B channels.
+
+    Args:
+        file_handle: File handle pointing to the opened HDF5 file from which
+            to load the data.
+        slices: Optional tuples of slices to use to subset the data to load.
+
+    Return:
+        A numpy.ndarray containing the loaded data.
+    """
+    chans = [
+        "89.0GHz-B,H", "89.0GHz-B,V",
+    ]
+    chans = [
+        f"Brightness Temperature ({freq})" for freq in chans
+    ]
+    return load_channels(file_handle, chans, slices=slices)
+
+
+def load_scan_time_amsr2(vrbl, slices=None) -> np.ndarray:
+    """
+    Load scan time into datetime64 array.
+
+    Args:
+        vrbl: Pointer to the HDF5 variable containing the scan time in
+            seconds.
+
+    Return:
+        A datetime64 array containing the absolute scan time.
+    """
+    if slices is None:
+        slices = slice(0, None)
+    d_t = vrbl.__getitem__(slices).astype("timedelta64[s]")
+    return np.datetime64("1993-01-01T00:00:00") + d_t
+
+
+l1b_gaasp_gcomw1_amsr2 = GAASPProduct()
+l1b_gcomw1_amsr2 = l1b_gaasp_gcomw1_amsr2
---
+++ test/unit_tests/products/noaa/test_gaasp.py
@@ -0,0 +1,59 @@
+"""
+Tests for the pansat.products.satellite.noaa.gaasp module.
+"""
+from datetime import datetime
+import os
+from pathlib import Path
+
+import pytest
+
+from pansat.catalog import Index
+from pansat.products.satellite.noaa.gaasp import l1b_gcomw1_amsr2
+
+
+NOAA_GAASP_DATA = os.environ.get("NOAA_GAASP_DATA")
+NEEDS_GAASP_DATA = pytest.mark.skipif(
+    NOAA_GAASP_DATA is None,
+    reason="Needs GAASP test data"
+)
+
+
+def test_match_filename():
+    """
+    Ensure that filename regexp matches actual filename.
+    """
+    fname = "GAASP-L1B_v2r2_GW1_s202112311612140_e202112311751120_c202112311858050.h5"
+    assert l1b_gcomw1_amsr2.matches(fname)
+
+
+def test_get_temporal_coverage():
+    """
+    Ensure that filename regexp matches actual filename.
+    """
+    fname = "GAASP-L1B_v2r2_GW1_s202112311612140_e202112311751120_c202112311858050.h5"
+    time_range = l1b_gcomw1_amsr2.get_temporal_coverage(fname)
+    assert time_range.start == datetime(2021, 12, 31, 16, 12, 14)
+    assert time_range.end == datetime(2021, 12, 31, 17, 51, 12)
+
+
+@NEEDS_GAASP_DATA
+def test_open():
+    """
+    Test opening of AMSR2 files.
+    """
+    files = sorted(list(Path(NOAA_GAASP_DATA).glob("*.h5")))
+    data = l1b_gcomw1_amsr2.open(files[0])
+    assert "tbs_s1" in data
+    assert "tbs_s2" in data
+    assert "tbs_s3" in data
+
+
+@NEEDS_GAASP_DATA
+def test_indexing():
+    """
+    Test indexing of AMSR2 files.
+    """
+    files = sorted(list(Path(NOAA_GAASP_DATA).glob("*.h5")))
+    assert len(files) > 0
+    index = Index.index(l1b_gcomw1_amsr2, files)
+    assert len(index) > 0
