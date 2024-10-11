b81be49cc8dec33654388084592f2647a785326f
Simon Pfreundschuh <s.pfreundschuh@gmail.com>
2024-10-11 08:07:18 -0600

    Extend tests for ERA5.

--- test/unit_tests/products/reanalysis/test_era5.py
+++ test/unit_tests/products/reanalysis/test_era5.py
@@ -2,39 +2,57 @@
 Tests for the pansat.products.reanalysis.era5 module.
 """
 
-from datetime import datetime
+from datetime import datetime, timedelta
 import random
 import os
 import sys
 import pytest
 import pansat.products.reanalysis.era5 as era5
+from pansat.geometry import LonLatRect
 
 
 PRODUCTS = [
-    era5.ERA5Hourly("surface", ["2m_temperature"]),
-    era5.ERA5Monthly("land", ["asn"], domain=[25, 50, 70, 120]),
+    era5.ERA5Hourly(levels="surface", variables=["2m_temperature"]),
+    era5.ERA5Monthly(levels="land",  variables=["asn"], domain=(70, 25, 120, 50)),
 ]
 
 TEST_NAMES = {
-    "reanalysis-era5-single-levels": "reanalysis-era5-single-levels_2016100115_2m_temperature.nc",
-    "reanalysis-era5-land-monthly-means": "reanalysis-era5-land-monthly-means_201610_asn25-50-70-120.nc",
+    "reanalysis.era5_surface_hourly_[2m_temperature]": "era5_surface_hourly_2016101500_2016101600_[2m_temperature].nc",
+    "reanalysis.era5_land_monthly_[asn]_[70,25,120,50]": "era5_land_monthly_201610_201611_[asn]_[70,25,120,50].nc",
 }
 
-
 TEST_TIMES = {
-    "reanalysis-era5-single-levels": datetime(2016, 10, 1, 15),
-    "reanalysis-era5-land-monthly-means": datetime(2016, 10, 1, 0),
+    "reanalysis.era5_surface_hourly_[2m_temperature]": datetime(2016, 10, 15),
+    "reanalysis.era5_land_monthly_[asn]_[70,25,120,50]": datetime(2016, 10, 1, 0),
+}
+
+TEST_DOMAINS = {
+    "reanalysis.era5_surface_hourly_[2m_temperature]": LonLatRect(-180, -90, 180, 90),
+    "reanalysis.era5_land_monthly_[asn]_[70,25,120,50]": LonLatRect(70, 25, 120, 50),
 }
 
 
 @pytest.mark.parametrize("product", PRODUCTS)
-def test_filename_to_date(product):
+def test_get_temporal_coverage(product):
     """
     Assert that time is correctly extracted from filename.
     """
     filename = TEST_NAMES[product.name]
-    time = product.filename_to_date(filename)
-    assert time == TEST_TIMES[product.name]
+    time_range = product.get_temporal_coverage(filename)
+    assert time_range.start == TEST_TIMES[product.name]
+
+
+@pytest.mark.parametrize("product", PRODUCTS)
+def test_get_spatial_coverage(product):
+    """
+    Assert that time is correctly extracted from filename.
+    """
+    filename = TEST_NAMES[product.name]
+    domain = product.get_spatial_coverage(filename)
+    bbox_era5 = domain.bounding_box_corners
+    bbox_ref = TEST_DOMAINS[product.name].bounding_box_corners
+    assert bbox_era5 == bbox_ref
+
 
 
 @pytest.mark.parametrize("product", PRODUCTS)
@@ -81,3 +99,12 @@ def test_download(tmpdir):
     filename = tmpdir / TEST_NAMES[product.name]
     data = product.open(str(filename))
     data.close()
+
+
+@pytest.mark.parametrize("product", PRODUCTS)
+def test_get_product(product):
+    """
+    Ensure that 'get_product' return the expected product object.
+    """
+    prod = era5.get_product(product.name)
+    assert prod.name == product.name
